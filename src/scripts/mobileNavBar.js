9eb6fc6d42d8d3b28200f6ca539012f3adcd2269
Andrey Marques <andreymarques822@gmail.com>
2022-09-07 02:02:56 -0300

    Frontend: Criação da funcionalidade do Burguer Botton para navbar mobile

---
+++ src/scripts/mobileNavBar.js
@@ -0,0 +1,6 @@
+function mobile() {
+    let button = document.getElementById("burguerButton")
+    let navbarDesktop = document.getElementById("navbarDesktop")
+    button.classList.toggle("is-active")
+    navbarDesktop.classList.toggle("is-active")
+}
