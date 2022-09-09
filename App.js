421ddc8dfcc0f3ea2a8279aa349e1ddd98c7b2a6
Andrey Marques <andreymarques822@gmail.com>
2022-09-08 22:37:32 -0300

    FrontEnd: Adicionando React ao Projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const App = () => {
+    return (
+        <Fragment>
+            <Routes/>
+        </Fragment>
+    )
+}
+//FrontEnd: função onde serão renderizadas as paginas frontend com react
+
+export default App
